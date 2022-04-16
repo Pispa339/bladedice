f8cb2cc4b13a443851770ece42d9548db951fd5d
Juho Pispa <juho.pispa@gmail.com>
2022-04-16 17:37:54 +0200

    WIP – Minor refactoring

--- BladeDice/TrickContext.swift
+++ BladeDice/TrickContext.swift
@@ -142,92 +142,44 @@ class TrickContext {
     func updateEnabledState(forTrickAttribute trickAttribute: Trick.TrickAttribute, isEnabled: Bool) {
         switch trickAttribute {
         case .trickType(let trickType):
+            updateEnabledStateForAllTrickAttributesForTrickType(trickType, isEnabled: isEnabled)
             switch trickType {
             case .soulPlate:
-                updateSoulPlateTricksEnabledState(isEnabled: isEnabled)
                 if trickAttributesByEnabledState[.trickType(trickType: .hBlock)] == false && !isEnabled {
-                    updateHBlockTricksEnabledState(isEnabled: true)
+                    updateEnabledStateForAllTrickAttributesForTrickType(.hBlock, isEnabled: true)
                 }
                 return
             case .hBlock:
-                updateHBlockTricksEnabledState(isEnabled: isEnabled)
                 if trickAttributesByEnabledState[.trickType(trickType: .soulPlate)] == false && !isEnabled {
-                    updateSoulPlateTricksEnabledState(isEnabled: true)
+                    updateEnabledStateForAllTrickAttributesForTrickType(.soulPlate, isEnabled: true)
                 }
                 return
             }
-        case .trickBase:
+        case .trickBase, .side, .spin:
             trickAttributesByEnabledState[trickAttribute] = isEnabled
-            if trickAttribute.isSoulPlateAttribute {
-                if enabledSoulPlateBases.count == 0 {
-                    trickAttributesByEnabledState[trickAttribute] = true
-                }
-            } else {
-                if enabledHBlockBases.count == 0 {
-                    trickAttributesByEnabledState[trickAttribute] = true
-                }
-            }
-        case .side:
-            trickAttributesByEnabledState[trickAttribute] = isEnabled
-            if trickAttribute.isSoulPlateAttribute {
-                if enabledSoulPlateSides.count == 0 {
-                    trickAttributesByEnabledState[trickAttribute] = true
-                }
-            } else {
-                if enabledHBlockSides.count == 0 {
-                    trickAttributesByEnabledState[trickAttribute] = true
-                }
-            }
-        case .spin:
-            trickAttributesByEnabledState[trickAttribute] = isEnabled
-            if trickAttribute.isSoulPlateAttribute {
-                if enabledSoulPlateTrickSpins.count == 0 {
-                    trickAttributesByEnabledState[trickAttribute] = true
-                }
-            } else {
-                if enabledHBlockTrickSpins.count == 0 {
-                    trickAttributesByEnabledState[trickAttribute] = true
-                }
+            if trickAttribute.isSoulPlateAttribute, enabledSoulPlateBases.count == 0 {
+                trickAttributesByEnabledState[trickAttribute] = true
+            } else if !trickAttribute.isSoulPlateAttribute, enabledHBlockBases.count == 0 {
+                trickAttributesByEnabledState[trickAttribute] = true
             }
         }
     }
     
-    private func updateSoulPlateTricksEnabledState(isEnabled: Bool) {
+    private func updateEnabledStateForAllTrickAttributesForTrickType(_ trickType: Trick.TrickType, isEnabled: Bool) {
+        let isSoulPlateTrickType = trickType == .soulPlate
         for (trickAttribute, isAttributeEnabled) in trickAttributesByEnabledState {
             switch trickAttribute {
             case .trickType(let trickType):
                 switch trickType {
                 case .soulPlate:
-                    trickAttributesByEnabledState[.trickType(trickType: .soulPlate)] = isEnabled
+                    trickAttributesByEnabledState[.trickType(trickType: .soulPlate)] = isSoulPlateTrickType ? isEnabled : isAttributeEnabled
                 case .hBlock:
-                    trickAttributesByEnabledState[.trickType(trickType: .hBlock)] = isAttributeEnabled
+                    trickAttributesByEnabledState[.trickType(trickType: .hBlock)] = isSoulPlateTrickType ? isAttributeEnabled : isEnabled
                 }
-            case .trickBase(let trickBase):
-                trickAttributesByEnabledState[trickAttribute] = trickBase.isSoulPlateTrick ? isEnabled : isAttributeEnabled
-            case .side(let side):
-                trickAttributesByEnabledState[trickAttribute] = side.isSoulPlateTrick ? isEnabled : isAttributeEnabled
-            case .spin(let spin):
-                trickAttributesByEnabledState[trickAttribute] = spin.isSoulPlateTrick ? isEnabled : isAttributeEnabled
-            }
-        }
-    }
-    
-    private func updateHBlockTricksEnabledState(isEnabled: Bool) {
-        for (trickAttribute, isAttributeEnabled) in trickAttributesByEnabledState {
-            switch trickAttribute {
-            case .trickType(let trickType):
-                switch trickType {
-                case .soulPlate:
-                    trickAttributesByEnabledState[.trickType(trickType: .soulPlate)] = isAttributeEnabled
-                case .hBlock:
-                    trickAttributesByEnabledState[.trickType(trickType: .hBlock)] = isEnabled
-                }
-            case .trickBase(let trickBase):
-                trickAttributesByEnabledState[trickAttribute] = trickBase.isSoulPlateTrick ? isAttributeEnabled : isEnabled
-            case .side(let side):
-                trickAttributesByEnabledState[trickAttribute] = side.isSoulPlateTrick ? isAttributeEnabled : isEnabled
-            case .spin(let spin):
-                trickAttributesByEnabledState[trickAttribute] = spin.isSoulPlateTrick ? isAttributeEnabled : isEnabled
+            case .trickBase, .side, .spin:
+                let isSoulPlateTypesEnabled = isSoulPlateTrickType ? isEnabled : isAttributeEnabled
+                let isHBlockTypesEnabled = !isSoulPlateTrickType ? isEnabled : isAttributeEnabled
+                trickAttributesByEnabledState[trickAttribute] = trickAttribute.isSoulPlateAttribute ? isSoulPlateTypesEnabled : isHBlockTypesEnabled
             }
         }
     }
